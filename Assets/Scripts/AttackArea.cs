90c48205d3911cf645bb3f894788ee730925231e
yogowang <wangyuci71@gmail.com>
2024-11-08 17:50:18 +0800

    now player attack can deal damage

--- Assets/Scripts/AttackArea.cs
+++ Assets/Scripts/AttackArea.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class AttackArea : MonoBehaviour
 {
-    private int damage = 3;
+    [SerializeField] private int damage;
 
 
     // Called when another collider enters this trigger collider
     private void OnTriggerEnter(Collider collider)
     {
         // Check if the collider has a Health component
-        Health health = collider.GetComponent<Health>();
+        //Health health = collider.GetComponent<Health>();
+        Health health = collider.GetComponentInParent<Health>();
         if (health != null)
         {
             // Apply damage to the health component
             health.Damage(damage);
+             Debug.Log("Dummy hit! Health reduced by " + damage);
         }
     }
 }
