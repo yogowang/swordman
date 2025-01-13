0d98fe03f7e569d1289fdc8bb3f4583869c9bfa5
yogowang <wangyuci71@gmail.com>
2025-01-13 12:43:51 +0800

    add new animation for enemy attack and animation controller and modified enemy ai so that it can attack

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,14 +6,23 @@ public class EnemyAI : MonoBehaviour
 {
     // Start is called before the first frame update
     private Transform player;
+    private Animator anim;
     void Start()
     {
          player = GameObject.Find("Player").transform;
+         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Attack();
+    }
+    private void Attack(){
+        if(anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack")){
+            return;
+        }
         transform.LookAt(player);
+        anim.Play("Attack", 0, 0);
     }
 }
