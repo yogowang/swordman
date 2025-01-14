98f4179a7002f844413937581f99a81bd86311d5
yogowang <wangyuci71@gmail.com>
2025-01-14 13:25:11 +0800

    add animation event so that enemy can deal damage

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,10 +7,12 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     private Transform player;
     private Animator anim;
+    private  Weapon weapon;
     void Start()
     {
          player = GameObject.Find("Player").transform;
          anim = GetComponent<Animator>();
+         weapon=GetComponentInChildren<Weapon>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,16 @@ public class EnemyAI : MonoBehaviour
             return;
         }
         transform.LookAt(player);
+        weapon.SetDamage(10);
         anim.Play("Attack", 0, 0);
+    }
+    public void EnableTriggerBox()
+    {
+        weapon.EnableTriggerBox();
+    }
+
+    public void DisableTriggerBox()
+    {
+        weapon.DisableTriggerBox();
     }
 }
