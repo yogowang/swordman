639732384172df871156762a6bf0a7ea0408fd16
Yuci Wang <yc457137@dal.ca>
2025-07-13 13:20:09 -0300

    add way point gizmos script

---
+++ Assets/Scripts/BehaviorTree/Tasks/Waypoints.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        foreach (Transform t in transform)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(t.position, 1f);
+        }
+        Gizmos.color = Color.red;
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position);
+        }
+        Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(0).position);
+    }
+}
